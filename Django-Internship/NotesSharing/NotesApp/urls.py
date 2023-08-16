9fb71b21b0e8f8e1567c94b830a427694c08c7c2
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2023-08-16 23:25:34 +0530

    day 38

--- Django-Internship/NotesSharing/NotesApp/urls.py
+++ Django-Internship/NotesSharing/NotesApp/urls.py
@@ -3,4 +3,7 @@ from NotesApp import views
 
 urlpatterns = [
 	path('',views.home,name="hm"),
+    path('abt/',views.about,name="ab"),
+	path('cnt/',views.contact,name="ct"),
+	path('reg/',views.register,name="rg"),
 ]
